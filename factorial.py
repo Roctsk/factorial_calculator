f9a8e20408571a9140eaebaa21ffccff50d42f87
Roctsk <rostyk20166@gmail.com>
2025-08-02 09:31:31 +0300

    Додав функцію факторіала і .gitignore

---
+++ factorial.py
@@ -0,0 +1,15 @@
+def calculator_factorial(n):
+    if n < 0:
+        return "Факторіал не визначений для від’ємних чисел."
+    result = 1
+    for i in range(1,n +1):
+        result *= i
+    return result
+
+if __name__== "__main__":
+    try:
+        number = int(input("Введіть ціле число: "))
+        print(f"Факторіал числа {number} дорівнює {calculator_factorial(number)}")
+    except ValueError:
+        print("Помилка: введіть ціле число.")
+            
